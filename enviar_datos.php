9b72ac539c35c6f03d2799a58c1c0fe5828d8cd7
rlaura <34401989+rlaura@users.noreply.github.com>
2019-05-29 01:40:59 -0300

    Añadido lógica del log in
    Realiza las consultas SQL de usuario, rol, listas, etc. 
    Valida el ingreso de usuarios y el rol.

--- enviar_datos.php
+++ enviar_datos.php
@@ -1,39 +1,63 @@
 <?php
 error_reporting(E_ALL ^ E_NOTICE);
 
-
+// Comienzo de la sesión
+session_start();
 $usuario = $_POST['usuario'];
 $pass = $_POST['pass'];
 
-$bd_comedor = mysqli_connect("localhost", "root", "", "comedor");
+// Guardar datos de sesión
+$_SESSION["usuario"] = $usuario;
+$_SESSION["pass"] = $pass;
 
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "usuarios";
-
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
+include("conexion.php");
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 } 
 
-$sql = "SELECT usuario, pass FROM usuarios where usuario = '$usuario' and pass = '$pass'";
-$result = $conn->query($sql);
-
-if ($result->num_rows > 0) {
-    // output data of each row
-
-	header("Location: principal.html");
-
-    
-} else {
-	 echo "<script>
-	                alert('Usuario incorrecto');
-                window.location= 'index.html'
-    </script>";
+/* comprobar la conexión */
+if ($conn->connect_errno) {
+  printf("Falló la conexión: %s\n", $mysqli->connect_error);
+  exit();
 }
+	// chequea que el usuario y el pass coincidan.
+	$sql = mysqli_query($conn,"SELECT usuario FROM usuarios where usuario = '$usuario' and pass = '$pass'");
+	if ($sql->num_rows > 0) {
+		// si la búsqueda dio por lo menos 1 fila es porque existe el usuario con ese pass. chequea si el usuario está activo.
+    	$result = mysqli_query($conn, "SELECT activo from usuarios where usuario = '$usuario' and pass = '$pass'");
+		$row = mysqli_fetch_assoc($result);
+		$activo = $row['activo']; // guarda el resultado en una variable.
+			
+			if ($activo == 0) { // si el usuario está inactivo, lo envía al index con un error para mostrar en pantalla.
+				header("Location:index.php?est=2");
+
+	 			// echo "<script>alert('Usuario inactivo. Tiene que verificar su cuenta.');			                window.location= 'index.php?activo=0'</script>";
+			} else {
+				// Busca el rol 
+			$result = mysqli_query($conn, "SELECT rol FROM usuarios WHERE usuario = '$usuario' and pass = '$pass'");
+			$row = mysqli_fetch_assoc($result);
+			$rol = $row['rol'];
+			$_SESSION["rol"] = $rol; // guarda el rol en la sesión.
+			
+				// si es otro tipo de usuarios, busca las listas activas
+				$sql1 = "SELECT id FROM listas ";
+				$result1 = $conn->query($sql1);	
+				$_SESSION['cantidad'] =$result1->num_rows; // guarda la cantidad de listas activas
+
+				$result = mysqli_query($conn, "SELECT nombre FROM menu WHERE seleccion = 1");
+				$row = mysqli_fetch_assoc($result);
+				$_SESSION['seleccion'] = $row['nombre'];
+				$seleccion = $_SESSION['seleccion'];
+			
+				header("Location:principal.php?cantidad=$cantidad&seleccion=$seleccion&usuario=$usuario&rol=$rol");
+				
+			}
+			
+	} else {
+						header("Location:index.php?est=1");
+}
+
 
 $conn->close();
 
