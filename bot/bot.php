0fef5eb742ea31927749628d267572481abcd20a
rlaura <34401989+rlaura@users.noreply.github.com>
2019-09-04 19:06:56 -0300

    Update bot.php

--- bot/bot.php
+++ bot/bot.php
@@ -2,10 +2,10 @@
 require_once('TwitterAPIExchange.php');
 
 $settings = array(
-    'oauth_access_token' => '1161801486063538177-5SfvvlED1PrZATj3PPaBdrkDjEzVlL',
-    'oauth_access_token_secret' => 'LdzpTM05puTWxk79N3DXsaeAKRgnG2isl1F9Vnl7fruTl',
-    'consumer_key' => 'DciCF9fMGrsDhNO7ItaXHWPw0',
-    'consumer_secret' => 'epX4yTJcJ3udDt2qbOq9IrYj9NAHXzIxdLWyq1bVmyHvINsNUE',
+    'oauth_access_token' => '',
+    'oauth_access_token_secret' => '',
+    'consumer_key' => '',
+    'consumer_secret' => '',
     );
 
 $url = "https://api.twitter.com/1.1/statuses/update.json";
